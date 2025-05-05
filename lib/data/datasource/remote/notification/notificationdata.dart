7d5f6160becd0411801e5eb691cab2b102bff578
abdulwahed-s <abdulwahedaldaghir0@gmail.com>
2025-05-05 22:18:43 +0400

    Add NotificationData class for managing notifications

---
+++ lib/data/datasource/remote/notification/notificationdata.dart
@@ -0,0 +1,21 @@
+import 'package:sire/apilink.dart';
+import 'package:sire/core/class/curd.dart';
+
+class NotificationData {
+  Curd curd;
+  NotificationData(this.curd);
+  getNotification(String userid) async {
+    var resp = await curd.postData(AppLink.viewNotification, {
+      "id": userid,
+    });
+    return resp.fold((s) => s, (r) => r);
+  }
+
+  deleteNotification(String userid, String notificationID) async {
+    var resp = await curd.postData(AppLink.deleteNotification, {
+      "userID": userid,
+      "notificationID": notificationID,
+    });
+    return resp.fold((s) => s, (r) => r);
+  }
+}
