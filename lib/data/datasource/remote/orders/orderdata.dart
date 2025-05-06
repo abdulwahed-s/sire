cede2018458b2206de8f96424beb6adf74a99ade
abdulwahed-s <abdulwahedaldaghir0@gmail.com>
2025-05-06 04:50:23 +0400

    Add OrderData class for fetching pending orders

---
+++ lib/data/datasource/remote/orders/orderdata.dart
@@ -0,0 +1,13 @@
+import 'package:sire/apilink.dart';
+import 'package:sire/core/class/curd.dart';
+
+class OrderData {
+  Curd curd;
+  OrderData(this.curd);
+  getPendingOrders(String userid) async {
+    var resp = await curd.postData(AppLink.viewPendingOrders, {
+      "userID": userid,
+    });
+    return resp.fold((s) => s, (r) => r);
+  }
+}
