c002ecf12ccaaecce4ba939da93353e11516f1a3
abdulwahed-s <abdulwahedaldaghir0@gmail.com>
2025-05-09 23:26:00 +0400

    feat: implement rating functionality with addRating, getRating, and getIsOrdered methods

--- lib/controller/items/itemsdetailsController.dart
+++ lib/controller/items/itemsdetailsController.dart
@@ -1,25 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:sire/controller/items/itemsController.dart';
 import 'package:sire/core/class/statusrequest.dart';
 import 'package:sire/core/constant/color.dart';
 import 'package:sire/core/functions/handlingdata.dart';
 import 'package:sire/core/services/services.dart';
 import 'package:sire/data/datasource/remote/cart/cartdata.dart';
+import 'package:sire/data/datasource/remote/rating/ratingdata.dart';
+import 'package:sire/data/model/ratingmodel.dart';
+import 'package:sire/view/widgets/items/form.dart';
 
 abstract class ItemsDetailsController extends GetxController {
   initiateData();
   addCart(String itemId);
   add();
   remove();
+  addRating(String itemid, String stars, String comment);
+  getRating();
+  getIsOrdered();
 }
 
 class ItemsDetailsControllerImp extends ItemsDetailsController {
-  // CartControllerImp cartcontroller = Get.put(CartControllerImp());
   int counter = 1;
   var data;
   late StatusRequest statusRequest;
   CartData cartData = CartData(Get.find());
   Services services = Get.find();
+  RatingData ratingData = RatingData(Get.find());
+  TextEditingController? comment;
+  final GlobalKey<AnimatedCommentFieldState> commentKey = GlobalKey();
+  double stars = 3;
+  List<RatingModel> allRating = [];
+  bool isOrdered = false;
+  late StatusRequest ratingStatusRequest;
 
   @override
   initiateData() async {
@@ -70,6 +83,66 @@ class ItemsDetailsControllerImp extends ItemsDetailsController {
   @override
   void onInit() {
     initiateData();
+    getRating();
+    getIsOrdered();
+    comment = TextEditingController();
     super.onInit();
+  }
+
+  @override
+  addRating(itemid, stars, comment) async {
+    statusRequest = StatusRequest.loding;
+    var response = await ratingData.addRating(
+      services.sharedPreferences.getString("id")!,
+      itemid,
+      stars,
+      comment,
+    );
+    statusRequest = handlingdata(response);
+    if (statusRequest == StatusRequest.success) {
+      if (response["status"] == "success") {
+        Get.back();
+        Get.find<ItemscontrollerImp>().statusRequest = StatusRequest.loding;
+        Get.find<ItemscontrollerImp>().getData("${data.itemCat}");
+      } else if (response["status"] == "failure") {
+        statusRequest = StatusRequest.failure;
+      }
+    }
+    update();
+  }
+
+  @override
+  getRating() async {
+    ratingStatusRequest = StatusRequest.loding;
+    allRating.clear();
+    var response = await ratingData.getRating(data.itemId.toString());
+    ratingStatusRequest = handlingdata(response);
+    if (ratingStatusRequest == StatusRequest.success) {
+      if (response["status"] == "success") {
+        List data = response['data'];
+        allRating.addAll(
+          data.map(
+            (e) => RatingModel.fromJson(e),
+          ),
+        );
+      } else if (response["status"] == "failure") {
+        ratingStatusRequest = StatusRequest.failure;
+      }
+    }
+    update();
+  }
+
+  @override
+  getIsOrdered() async {
+    var response = await ratingData.isOrdered(
+      services.sharedPreferences.getString("id")!,
+      data.itemId.toString(),
+    );
+    if (response["status"] == "success") {
+      isOrdered = true;
+    } else if (response["status"] == "failure") {
+      isOrdered = false;
+    }
+    update();
   }
 }
