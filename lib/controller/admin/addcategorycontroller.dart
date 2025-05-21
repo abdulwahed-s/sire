a58696c25dd5ae6ec389a50a26516e26fc5a8835
abdulwahed-s <abdulwahedaldaghir0@gmail.com>
2025-05-21 18:36:13 +0400

    feat: implement AddCategoryController for category management

---
+++ lib/controller/admin/addcategorycontroller.dart
@@ -0,0 +1,83 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:sire/controller/admin/admincategorycontroller.dart';
+import 'package:sire/core/class/statusrequest.dart';
+import 'package:sire/core/constant/color.dart';
+import 'package:sire/core/functions/addimage.dart';
+import 'package:sire/core/functions/handlingdata.dart';
+import 'package:sire/data/datasource/remote/admin/admindata.dart';
+
+abstract class AddCategoryController extends GetxController {
+  addCategory();
+}
+
+class AddCategoryControllerImp extends AddCategoryController {
+  TextEditingController? categoryName;
+  TextEditingController? categoryNameAr;
+  TextEditingController? categoryNameEs;
+
+  GlobalKey<FormState> formstate = GlobalKey<FormState>();
+
+  AdminData adminData = AdminData(Get.find());
+  StatusRequest statusRequest = StatusRequest.none;
+
+  File? image;
+
+  getImage() async {
+    image = await uploadImage();
+    update();
+  }
+
+  @override
+  addCategory() async {
+    if (!formstate.currentState!.validate()) {
+      return;
+    }
+    if (image == null) {
+      Get.snackbar(
+        "Image",
+        "Please add image",
+        colorText: Appcolor.charcoalGray,
+        backgroundColor: Appcolor.rosePompadour,
+        icon: const Icon(Icons.error),
+      );
+      return;
+    }
+    statusRequest = StatusRequest.loding;
+    update();
+    var response = await adminData.addCategory(
+      categoryName!.text,
+      categoryNameAr!.text,
+      categoryNameEs!.text,
+      image!,
+    );
+    statusRequest = handlingdata(response);
+    print(response);
+    if (statusRequest == StatusRequest.success) {
+      if (response["status"] == "success") {
+        Get.find<AdminCategoryControllerImp>().getCategories();
+        Get.back();
+        Get.snackbar(
+          "Success",
+          "Category added successfully",
+          colorText: Appcolor.charcoalGray,
+          backgroundColor: Appcolor.rosePompadour,
+          icon: const Icon(Icons.check_circle),
+        );
+      } else if (response["status"] == "failure") {
+        statusRequest = StatusRequest.failure;
+      }
+    }
+    update();
+  }
+
+  @override
+  void onInit() {
+    categoryName = TextEditingController();
+    categoryNameAr = TextEditingController();
+    categoryNameEs = TextEditingController();
+    super.onInit();
+  }
+}
