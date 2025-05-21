42facb6025a4910021bc6bef239fde1ae5668e6f
abdulwahed-s <abdulwahedaldaghir0@gmail.com>
2025-05-21 18:37:59 +0400

    feat: add CategoryNameField, Header, SaveButton, and SelectImage widgets for admin interface

---
+++ lib/view/widgets/admin/savebutton.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:sire/core/class/statusrequest.dart';
+import 'package:sire/core/constant/color.dart';
+import 'package:sire/view/widgets/address/gradientprogressindicator.dart';
+
+class SaveButton extends StatelessWidget {
+  final void Function()? onPressed;
+  final StatusRequest statusRequest;
+  const SaveButton({super.key, this.onPressed, required this.statusRequest});
+
+  @override
+  Widget build(BuildContext context) {
+    return IgnorePointer(
+      ignoring: statusRequest == StatusRequest.loding,
+      child: AnimatedContainer(
+        duration: Duration(milliseconds: 300),
+        curve: Curves.easeInOut,
+        decoration: BoxDecoration(
+          color: statusRequest == StatusRequest.loding
+              ? Appcolor.grey
+              : Appcolor.pink,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            elevation: 2,
+            padding: const EdgeInsets.symmetric(vertical: 16),
+            backgroundColor: Colors.transparent, // Set transparent here
+            shadowColor: Colors.transparent, // So it doesn't double shadow
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+          ),
+          onPressed: onPressed,
+          child: SizedBox(
+            height: 28,
+            child: statusRequest == StatusRequest.loding
+                ? GradientProgressIndicator(strokeWidth: 2)
+                : Text(
+                    'Save Category',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+          ),
+        ),
+      ),
+    );
+  }
+}
