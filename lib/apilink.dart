d5afe4e4363f7f9a693a797efd9fabb521dbe5ae
abdulwahed-s <abdulwahedaldaghir0@gmail.com>
2025-05-22 23:25:30 +0400

    feat: add admin API endpoints for managing categories and items

--- lib/apilink.dart
+++ lib/apilink.dart
@@ -63,11 +63,16 @@ static const String acceptorder = "$server/delivery/acceptorder.php";
 static const String viewAcceptedOrders = "$server/delivery/viewaccepted.php";
 static const String markAsDelivered = "$server/delivery/markasdelivered.php";
 //admin
+//categories
 static const String getCategories = "$server/admin/categories/view.php";
 static const String addCategories = "$server/admin/categories/add.php";
 static const String updateCategories = "$server/admin/categories/update.php";
 static const String deleteCategories = "$server/admin/categories/delete.php";
-
+//items
+static const String getItems = "$server/admin/items/view.php";
+static const String addItems = "$server/admin/items/add.php";
+static const String updateItems = "$server/admin/items/update.php";
+static const String deleteItems = "$server/admin/items/delete.php";
 
 
 
