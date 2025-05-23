9f3c1682edae0229b3fa72911d9be0f14b7695d2
abdulwahed-s <abdulwahedaldaghir0@gmail.com>
2025-05-23 11:41:24 +0400

    feat: implement item management methods in AdminData class

--- lib/data/datasource/remote/admin/admindata.dart
+++ lib/data/datasource/remote/admin/admindata.dart
@@ -69,4 +69,106 @@ class AdminData {
 
     return resp.fold((s) => s, (r) => r);
   }
+
+  getItems() async {
+    var resp = await curd.postData(AppLink.getItems, {});
+    return resp.fold((s) => s, (r) => r);
+  }
+
+  addItem(
+    String name,
+    String namear,
+    String namees,
+    String desc,
+    String descar,
+    String desces,
+    String count,
+    String active,
+    String price,
+    String discount,
+    String catgoryId,
+    File file,
+  ) async {
+    var resp = await curd.addRequestWithImageOne(
+      AppLink.addItems,
+      {
+        "name": name,
+        "namear": namear,
+        "namees": namees,
+        "desc": desc,
+        "descar": descar,
+        "desces": desces,
+        "count": count,
+        "active": active,
+        "price": price,
+        "discount": discount,
+        "catid": catgoryId,
+      },
+      file,
+    );
+    return resp.fold((s) => s, (r) => r);
+  }
+
+  deleteItem(String id, String imgname) async {
+    var resp = await curd.postData(AppLink.deleteItems, {
+      "id": id,
+      "imgname": imgname,
+    });
+    return resp.fold((s) => s, (r) => r);
+  }
+
+  updateItem(
+    String id,
+    String name,
+    String namear,
+    String namees,
+    String desc,
+    String descar,
+    String desces,
+    String count,
+    String active,
+    String price,
+    String discount,
+    String catgoryId,
+    File? file,
+    String oldimg,
+  ) async {
+    var resp;
+    if (file == null) {
+      resp = await curd.postData(AppLink.updateItems, {
+        "id": id,
+        "name": name,
+        "namear": namear,
+        "namees": namees,
+        "desc": desc,
+        "descar": descar,
+        "desces": desces,
+        "count": count,
+        "active": active,
+        "price": price,
+        "discount": discount,
+        "catid": catgoryId
+      });
+    } else {
+      resp = await curd.addRequestWithImageOne(
+          AppLink.updateItems,
+          {
+            "id": id,
+            "name": name,
+            "namear": namear,
+            "namees": namees,
+            "desc": desc,
+            "descar": descar,
+            "desces": desces,
+            "count": count,
+            "active": active,
+            "price": price,
+            "discount": discount,
+            "catid": catgoryId,
+            "oldimg": oldimg,
+          },
+          file);
+    }
+    return resp.fold((s) => s, (r) => r);
+  }
 }
