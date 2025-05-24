e967b80438fc0b3b4def94ef99fdf1a780bd2de6
abdulwahed-s <abdulwahedaldaghir0@gmail.com>
2025-05-24 21:29:10 +0400

    feat: add AdminOrderDetails screen with order summary, items, and delivery details

---
+++ lib/view/widgets/admin/SectionHeader.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:sire/core/constant/color.dart';
+
+class SectionHeader extends StatelessWidget {
+  final String title;
+  const SectionHeader({super.key, required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      title,
+      style: TextStyle(
+        fontSize: 18,
+        fontWeight: FontWeight.bold,
+        color: Appcolor.berry,
+      ),
+    );
+  }
+}
