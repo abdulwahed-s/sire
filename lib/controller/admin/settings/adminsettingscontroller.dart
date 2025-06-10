11060f1975ce5614cc1bcd6fcd32f5c05eaa235c
abdulwahed-s <abdulwahedaldaghir0@gmail.com>
2025-06-10 05:14:35 +0400

    feat: implement AdminSettingsController for managing admin settings and navigation

---
+++ lib/controller/admin/settings/adminsettingscontroller.dart
@@ -0,0 +1,78 @@
+import 'package:get/get.dart';
+import 'package:sire/view/screens/admin/settings/createnewadminaccount.dart';
+import 'package:sire/core/functions/disablenotification.dart';
+import 'package:sire/core/services/services.dart';
+import 'package:sire/view/screens/admin/settings/createnewdeliveryaccount.dart';
+import 'package:sire/view/screens/admin/settings/updateaccountinformation.dart';
+
+abstract class AdminSettingsController extends GetxController {
+  disableNotification();
+  goToCreateNewDeliveryAccount();
+  goToCreateNewAdminAccount();
+  goToUpdateAccountInformation();
+  updateAccount();
+}
+
+class AdminSettingsControllerImp extends AdminSettingsController {
+  Services services = Get.find();
+
+  String? username;
+  String? email;
+  String? pfp;
+  String? banner;
+
+  bool? isNotificationEnabled;
+
+  @override
+  void onInit() {
+    username = services.sharedPreferences.getString("username")!;
+    email = services.sharedPreferences.getString("email")!;
+    pfp = services.sharedPreferences.getString("pfp")!;
+    banner = services.sharedPreferences.getString("banner")!;
+    isNotificationEnabled =
+        services.sharedPreferences.getBool("isNotificationEnabled");
+
+    super.onInit();
+  }
+
+  @override
+  disableNotification() {
+    disableNotifications();
+    isNotificationEnabled =
+        services.sharedPreferences.getBool("isNotificationEnabled");
+    update();
+  }
+
+  @override
+  goToCreateNewDeliveryAccount() {
+    Get.to(
+      () => CreateNewDeliveryAccount(),
+      transition: Transition.leftToRight,
+    );
+  }
+
+  @override
+  goToCreateNewAdminAccount() {
+    Get.to(
+      () => CreateNewAdminAccount(),
+      transition: Transition.leftToRight,
+    );
+  }
+
+  @override
+  goToUpdateAccountInformation() {
+    Get.to(
+      () => UpdateAccountInformation(),
+      transition: Transition.leftToRight,
+    );
+  }
+
+  @override
+  updateAccount() {
+    username = services.sharedPreferences.getString("username")!;
+    email = services.sharedPreferences.getString("email")!;
+    pfp = services.sharedPreferences.getString("pfp")!;
+    banner = services.sharedPreferences.getString("banner")!;
+    update();
+  }
+}
