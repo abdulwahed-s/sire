406336caa8545cdb07ee8f5ab433740b8d92dcb4
abdulwahed-s <abdulwahedaldaghir0@gmail.com>
2025-06-18 19:04:04 +0400

    feat: implement NavItem widget for enhanced navigation with animations and notifications

--- lib/view/widgets/delivery/deliverybottomnavigationbar.dart
+++ lib/view/widgets/delivery/deliverybottomnavigationbar.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:sire/controller/delivery/deliveryhomecontroller.dart';
 import 'package:sire/core/constant/color.dart';
+import 'package:sire/view/widgets/delivery/navitem.dart';
 
 class DeliveryBottomNavigationBar extends StatelessWidget {
   const DeliveryBottomNavigationBar({super.key});
@@ -11,75 +13,55 @@ class DeliveryBottomNavigationBar extends StatelessWidget {
     return GetBuilder<DeliveryHomeControllerImp>(
       builder: (controller) {
         return Container(
+          margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+          height: 106,
           decoration: BoxDecoration(
             color: Colors.white,
+            borderRadius: BorderRadius.circular(28),
+            border: Border.all(
+              color: Colors.grey.shade100,
+              width: 1,
+            ),
             boxShadow: [
               BoxShadow(
-                color: Colors.grey.withValues(alpha: 0.3),
-                blurRadius: 10,
-                offset: const Offset(0, -2),
+                color: Colors.black.withValues(alpha: 0.08),
+                blurRadius: 24,
+                offset: const Offset(0, 12),
+                spreadRadius: -4,
+              ),
+              BoxShadow(
+                color: Appcolor.berry.withValues(alpha: 0.08),
+                blurRadius: 40,
+                offset: const Offset(0, 8),
+                spreadRadius: -8,
               ),
             ],
           ),
-          child: BottomAppBar(
-            height: 70,
-            notchMargin: 8,
-            elevation: 0,
-            color: Colors.transparent,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: List.generate(
-                controller.listpages.length,
-                (index) => _buildNavItem(
-                  context,
-                  icon: controller.iconpages[index],
-                  label: controller.namepages[index],
-                  isActive: controller.currentpage == index,
-                  onTap: () => controller.changePage(index),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(28),
+            child: Material(
+              color: Colors.transparent,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: List.generate(
+                  controller.listpages.length,
+                  (index) => NavItem(
+                    controller: controller,
+                    index: index,
+                    icon: controller.iconpages[index],
+                    label: controller.namepages[index],
+                    isActive: controller.currentpage == index,
+                    onTap: () {
+                      HapticFeedback.lightImpact();
+                      controller.changePage(index);
+                    },
+                  ),
                 ),
               ),
             ),
           ),
         );
       },
-    );
-  }
-
-  Widget _buildNavItem(
-    BuildContext context, {
-    required IconData icon,
-    required String label,
-    required bool isActive,
-    required VoidCallback onTap,
-  }) {
-    return Expanded(
-      child: Material(
-        color: Colors.transparent,
-        child: InkWell(
-          onTap: onTap,
-          splashColor: Appcolor.berry.withValues(alpha: 0.2),
-          highlightColor: Appcolor.berry.withValues(alpha: 0.1),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Icon(
-                icon,
-                size: 24,
-                color: isActive ? Appcolor.berry : Colors.grey[600],
-              ),
-              const SizedBox(height: 4),
-              Text(
-                label,
-                style: TextStyle(
-                  fontSize: 12,
-                  fontWeight: isActive ? FontWeight.w600 : FontWeight.w500,
-                  color: isActive ? Appcolor.berry : Colors.grey[600],
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
     );
   }
 }
