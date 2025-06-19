64c8f7afc13a8eda14fbd17d781c661bb2b02ef4
abdulwahed-s <abdulwahedaldaghir0@gmail.com>
2025-06-19 17:25:52 +0400

    feat: update VerifyCodeSignUpController to handle snackbar visibility and improve code structure

--- lib/controller/auth/verifyCodeSignUpController.dart
+++ lib/controller/auth/verifyCodeSignUpController.dart
@@ -3,18 +3,21 @@ import 'package:get/get.dart';
 import 'package:sire/core/class/statusrequest.dart';
 import 'package:sire/core/constant/color.dart';
 import 'package:sire/core/functions/handlingdata.dart';
+import 'package:sire/core/services/services.dart';
 import 'package:sire/data/datasource/remote/auth/verifycodedata.dart';
-import 'package:sire/view/screens/home/home.dart';
+import 'package:sire/view/screens/home/homescreen.dart';
 
 abstract class VerifyCodeSignUpController extends GetxController {
   checkCode(String verify);
-  resendCode();
+  resendCode({bool disableSnackbar = false});
 }
 
 class VerifyCodeSignUpControllerImp extends VerifyCodeSignUpController {
   String? email;
+  bool? isFromSetting;
   StatusRequest statusRequest = StatusRequest.none;
   VerifycodeData verifycodeData = VerifycodeData(Get.find());
+  Services services = Get.find();
 
   @override
   checkCode(String verify) async {
@@ -25,7 +28,8 @@ class VerifyCodeSignUpControllerImp extends VerifyCodeSignUpController {
     if (statusRequest == StatusRequest.success) {
       if (response["status"] == "success") {
         // data.addAll(response['data']);
-        Get.offAll(() => Home(),
+        services.sharedPreferences.setString("approve", "1");
+        Get.offAll(() => HomeScreen(),
             transition: Transition.rightToLeft,
             duration: Duration(milliseconds: 800));
       } else if (response["status"] == "failure") {
@@ -40,20 +44,22 @@ class VerifyCodeSignUpControllerImp extends VerifyCodeSignUpController {
   }
 
   @override
-  resendCode() async {
+  resendCode({bool disableSnackbar = false}) async {
     statusRequest = StatusRequest.loding;
     update();
     var response = await verifycodeData.resendCode(email!);
     statusRequest = handlingdata(response);
     if (statusRequest == StatusRequest.success) {
       if (response["status"] == "success") {
-        Get.snackbar(
-          "Verification code resent successfully.",
-          "Please check your email for the new code",
-          colorText: Appcolor.charcoalGray,
-          backgroundColor: Appcolor.rosePompadour,
-          icon: const Icon(Icons.email_rounded),
-        );
+        if (!disableSnackbar) {
+          Get.snackbar(
+            "Verification code resent successfully.",
+            "Please check your email for the new code",
+            colorText: Appcolor.charcoalGray,
+            backgroundColor: Appcolor.rosePompadour,
+            icon: const Icon(Icons.email_rounded),
+          );
+        }
       } else if (response["status"] == "failure") {
         statusRequest = StatusRequest.failure;
         Get.defaultDialog(
@@ -67,6 +73,12 @@ class VerifyCodeSignUpControllerImp extends VerifyCodeSignUpController {
   @override
   void onInit() {
     email = Get.arguments["email"];
+    isFromSetting = Get.arguments?["setting"] ?? false;
+
+    if (isFromSetting!) {
+      resendCode(disableSnackbar: true);
+    }
+
     super.onInit();
   }
 }
