72ac5ab4e6045583c31b2ee4ae5f32410fa1a898
abdulwahed-s <abdulwahedaldaghir0@gmail.com>
2025-06-23 05:35:26 +0400

    feat: extend DashboardModel with new data classes for sales, categories, and delivery workers

--- lib/data/model/dashboardmodel.dart
+++ lib/data/model/dashboardmodel.dart
@@ -6,6 +6,11 @@ class DashboardModel {
   int? customer;
   List<Orders>? orders;
   List<TopProducts>? topProducts;
+  List<SalesOverWeek>? salesOverWeek;
+  List<TopCategories>? topCategories;
+  List<SalesOverMonth>? salesOverMonth;
+  List<OrdersNumber>? ordersNumber;
+  List<DeliveryWorkers>? deliveryWorkers;
 
   DashboardModel(
       {this.status,
@@ -14,7 +19,12 @@ class DashboardModel {
       this.items,
       this.customer,
       this.orders,
-      this.topProducts});
+      this.topProducts,
+      this.salesOverWeek,
+      this.topCategories,
+      this.salesOverMonth,
+      this.ordersNumber,
+      this.deliveryWorkers});
 
   DashboardModel.fromJson(Map<String, dynamic> json) {
     status = json['status'];
@@ -34,6 +44,36 @@ class DashboardModel {
         topProducts!.add(TopProducts.fromJson(v));
       });
     }
+    if (json['salesOverWeek'] != null) {
+      salesOverWeek = <SalesOverWeek>[];
+      json['salesOverWeek'].forEach((v) {
+        salesOverWeek!.add(SalesOverWeek.fromJson(v));
+      });
+    }
+    if (json['topCategories'] != null) {
+      topCategories = <TopCategories>[];
+      json['topCategories'].forEach((v) {
+        topCategories!.add(TopCategories.fromJson(v));
+      });
+    }
+    if (json['salesOverMonth'] != null) {
+      salesOverMonth = <SalesOverMonth>[];
+      json['salesOverMonth'].forEach((v) {
+        salesOverMonth!.add(SalesOverMonth.fromJson(v));
+      });
+    }
+    if (json['Orders_Number'] != null) {
+      ordersNumber = <OrdersNumber>[];
+      json['Orders_Number'].forEach((v) {
+        ordersNumber!.add(OrdersNumber.fromJson(v));
+      });
+    }
+    if (json['Delivery_Workers'] != null) {
+      deliveryWorkers = <DeliveryWorkers>[];
+      json['Delivery_Workers'].forEach((v) {
+        deliveryWorkers!.add(DeliveryWorkers.fromJson(v));
+      });
+    }
   }
 
   Map<String, dynamic> toJson() {
@@ -49,6 +89,26 @@ class DashboardModel {
     if (topProducts != null) {
       data['topProducts'] = topProducts!.map((v) => v.toJson()).toList();
     }
+    if (salesOverWeek != null) {
+      data['salesOverWeek'] =
+          salesOverWeek!.map((v) => v.toJson()).toList();
+    }
+    if (topCategories != null) {
+      data['topCategories'] =
+          topCategories!.map((v) => v.toJson()).toList();
+    }
+    if (salesOverMonth != null) {
+      data['salesOverMonth'] =
+          salesOverMonth!.map((v) => v.toJson()).toList();
+    }
+    if (ordersNumber != null) {
+      data['Orders_Number'] =
+          ordersNumber!.map((v) => v.toJson()).toList();
+    }
+    if (deliveryWorkers != null) {
+      data['Delivery_Workers'] =
+          deliveryWorkers!.map((v) => v.toJson()).toList();
+    }
     return data;
   }
 }
@@ -58,14 +118,21 @@ class Orders {
   int? orderTotalprice;
   double? orderStatus;
   String? userName;
+  String? userPfp;
 
-  Orders({this.orderId, this.orderTotalprice, this.orderStatus, this.userName});
+  Orders(
+      {this.orderId,
+      this.orderTotalprice,
+      this.orderStatus,
+      this.userName,
+      this.userPfp});
 
   Orders.fromJson(Map<String, dynamic> json) {
     orderId = json['order_id'];
     orderTotalprice = json['order_totalprice'];
     orderStatus = (json['order_status'] as num?)?.toDouble();
     userName = json['user_name'];
+    userPfp = json['user_pfp'];
   }
 
   Map<String, dynamic> toJson() {
@@ -74,6 +141,7 @@ class Orders {
     data['order_totalprice'] = orderTotalprice;
     data['order_status'] = orderStatus;
     data['user_name'] = userName;
+    data['user_pfp'] = userPfp;
     return data;
   }
 }
@@ -107,6 +175,111 @@ class TopProducts {
     data['item_discount'] = itemDiscount;
     data['item_img'] = itemImg;
     data['total_sold'] = totalSold;
+    return data;
+  }
+}
+
+class SalesOverWeek {
+  String? dayName;
+  String? totalSales;
+  int? orderCount;
+  int? sortOrder;
+
+  SalesOverWeek(
+      {this.dayName, this.totalSales, this.orderCount, this.sortOrder});
+
+  SalesOverWeek.fromJson(Map<String, dynamic> json) {
+    dayName = json['day_name'];
+    totalSales = json['total_sales'];
+    orderCount = json['order_count'];
+    sortOrder = json['sort_order'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['day_name'] = dayName;
+    data['total_sales'] = totalSales;
+    data['order_count'] = orderCount;
+    data['sort_order'] = sortOrder;
+    return data;
+  }
+}
+
+class TopCategories {
+  int? totalSelling;
+  String? categoryName;
+
+  TopCategories({this.totalSelling, this.categoryName});
+
+  TopCategories.fromJson(Map<String, dynamic> json) {
+    totalSelling = json['Total_Selling'];
+    categoryName = json['Category_name'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['Total_Selling'] = totalSelling;
+    data['Category_name'] = categoryName;
+    return data;
+  }
+}
+
+class SalesOverMonth {
+  String? monthShort;
+  String? totalSales;
+
+  SalesOverMonth({this.monthShort, this.totalSales});
+
+  SalesOverMonth.fromJson(Map<String, dynamic> json) {
+    monthShort = json['month_short'];
+    totalSales = json['total_sales'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['month_short'] = monthShort;
+    data['total_sales'] = totalSales;
+    return data;
+  }
+}
+
+class OrdersNumber {
+  int? ordersNumber;
+  double? orderStatus;
+
+  OrdersNumber({this.ordersNumber, this.orderStatus});
+
+  OrdersNumber.fromJson(Map<String, dynamic> json) {
+    ordersNumber = json['Orders_Number'];
+    orderStatus = (json['order_status']as num?)?.toDouble();
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['Orders_Number'] = ordersNumber;
+    data['order_status'] = orderStatus;
+    return data;
+  }
+}
+
+class DeliveryWorkers {
+  int? numberOfOrders;
+  String? userName;
+  String? userPfp;
+
+  DeliveryWorkers({this.numberOfOrders, this.userName, this.userPfp});
+
+  DeliveryWorkers.fromJson(Map<String, dynamic> json) {
+    numberOfOrders = json['Number_Of_Orders'];
+    userName = json['user_name'];
+    userPfp = json['user_pfp'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['Number_Of_Orders'] = numberOfOrders;
+    data['user_name'] = userName;
+    data['user_pfp'] = userPfp;
     return data;
   }
 }
