623862b0c3ae81c006b9ef17181191940108cdef
abdulwahed-s <abdulwahedaldaghir0@gmail.com>
2025-06-23 05:39:37 +0400

    feat: refactor ItemsView to use PageView for category navigation and simplify layout

--- lib/view/screens/items/ItemsView.dart
+++ lib/view/screens/items/ItemsView.dart
@@ -1,13 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:get/get.dart';
 import 'package:sire/controller/favourites/favouritesController.dart';
 import 'package:sire/controller/items/itemsController.dart';
-import 'package:sire/core/class/handlingdatareq.dart';
-import 'package:sire/core/constant/color.dart';
-import 'package:sire/data/model/itemsmodel.dart';
 import 'package:sire/view/widgets/items/CategorieslistItems.dart';
-import 'package:sire/view/widgets/items/customitemslist.dart';
+import 'package:sire/view/widgets/items/categorypagecontent.dart';
 
 class ItemsView extends StatelessWidget {
   const ItemsView({super.key});
@@ -17,41 +13,34 @@ class ItemsView extends StatelessWidget {
     Get.put(ItemscontrollerImp());
     FavouritesControllerImp favouritesController =
         Get.put(FavouritesControllerImp());
+
     return Scaffold(
-        backgroundColor: Appcolor.white,
-        body: GetBuilder<ItemscontrollerImp>(
-          builder: (controller) {
-            return HandlingDataRequest(
-                statusRequest: controller.statusRequest,
-                widget: ListView(
-                  children: [
-                    CategorieslistItems(),
-                    MasonryGridView.count(
-                      crossAxisCount: 2,
-                      mainAxisSpacing: 5,
-                      crossAxisSpacing: 5,
-                      itemCount: controller.data.length,
-                      shrinkWrap: true,
-                      physics: NeverScrollableScrollPhysics(),
-                      itemBuilder: (context, index) {
-                        favouritesController
-                                .favourites[controller.data[index]["item_id"]] =
-                            controller.data[index]["favourite"];
-                        return CustomItemsList(
-                          loading: controller
-                              .isLoadingItem(controller.data[index]["item_id"]),
-                          onTap: () {
-                            controller.addToCart(
-                                "${controller.data[index]["item_id"]}");
-                          },
-                          itemsModel:
-                              ItemsModel.fromJson(controller.data[index]),
-                        );
-                      },
-                    )
-                  ],
-                ));
-          },
-        ));
+      backgroundColor: Colors.grey[50],
+      body: GetBuilder<ItemscontrollerImp>(
+        builder: (controller) {
+          return Column(
+            children: [
+              const SizedBox(height: 50),
+              const CategorieslistItems(),
+              Expanded(
+                child: PageView.builder(
+                  controller: controller.pageController,
+                  onPageChanged: (index) {
+                    controller.onPageChanged(index);
+                  },
+                  itemCount: controller.categories.length,
+                  itemBuilder: (context, pageIndex) {
+                    return CategoryPageContent(
+                      categoryIndex: pageIndex,
+                      favouritesController: favouritesController,
+                    );
+                  },
+                ),
+              ),
+            ],
+          );
+        },
+      ),
+    );
   }
 }
